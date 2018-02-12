a6149a3d818f2c93c8695643686dc251a22132ab
stevenshoemaker <steven.shoemaker@wework.com>
2018-02-12 00:41:55 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,6 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
 board = [" " , " ", " ", " ", " ", " ", " " , " " , " "]
 
-def input_to_index(input)
+def position_taken(board,input)
   index = input.to_i - 1
 end
 
