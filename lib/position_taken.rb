981b5465356e225864fca74ba8d7968d8a9390df
stevenshoemaker <steven.shoemaker@wework.com>
2018-02-12 01:13:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken? (board,index)
 if board[index] == " " || board[index] == ""
     false
- elsif board[index] == "X" || board[index] "O"
+ elsif board[index] == "X" || board[index] == "O"
     true
   end
 end
