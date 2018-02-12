97436435075486bc75997972bd2303f3d71fe2ac
stevenshoemaker <steven.shoemaker@wework.com>
2018-02-12 00:42:37 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,5 @@
 board = [" " , " ", " ", " ", " ", " ", " " , " " , " "]
 
 def position_taken(board,input)
-  index = input.to_i - 1
-end
-
-def move (board, index, token = "X")
-  board[index] = token
+gets input.strip.to_i 
 end
